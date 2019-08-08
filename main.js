e2a03f800f57654258f6f465bc56ed686c296dbc
jvyden420 <jvyden420@gmail.com>
2019-08-07 22:00:14 -0400

    Nicely format output

--- main.js
+++ main.js
@@ -14,8 +14,21 @@ server.on('connection', function(socket) {
     socket.end(`HTTP/1.1 200\n\n${html}${getProcesses()}`)
   })
 })
+//USER       PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND
+//jvyden   22429  0.0  0.0  39664  3540 pts/2    R+   21:23   0:00 ps -aux
 
+// USER   %CPU %MEM COMMAND
+// jvyden 0.0 0.0 ssh
 function getProcesses() {
-  processes = child.execSync("ps -aux | grep jvyden").toString()
-  return processes
+  let psaux = child.execSync("ps -aux --no-headers | sort -nrk 3,3").toString()
+  processes = psaux.split('\n')
+  for(let i=0;i<processes.length;i++) {
+    let process = processes[i]
+    let pcolumns = process.split(" ")
+    while(pcolumns.includes('')) {for(let l=0;l<pcolumns.length;l++) {if(pcolumns[l].length === 0){pcolumns.splice(l,1)}}}
+    process = `${pcolumns[0]} ${pcolumns[2]} ${pcolumns[3]} ${pcolumns[10]}`
+    processes[i] = process
+  }
+  processes.pop()
+  return processes.join('\n')
 }
